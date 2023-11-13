19f90807b46c03adbdc76d477f0ebbc176a40eef
Jomary Coquilla <jomarycoquilla465@gmail.com>
2023-11-13 22:13:23 +0800

    .

---
+++ app/src/main/java/com/example/triplesagent/Nav.java
@@ -0,0 +1,17 @@
+package com.example.triplesagent;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.triplesagent.R;
+
+public class Nav extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_bar);
+
+    }
+}
